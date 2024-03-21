d49249bd6c151cb9e5ed495f931d1e99f202a51b
pcplazash <paulo.plazas@gmail.com>
2024-03-21 12:25:07 -0500

    march21

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Movies Catalogue',
       initialRoute: 'home',
       routes: {
         'home': (_) => const HomeScreen(),
